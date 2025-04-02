50c2de5cc028886685c23209a62600f8494a6849
MARTIN Nolann <nolann.martin.2003@gmail.com>
2025-04-02 09:32:17 +0200

    add

--- clustering.py
+++ clustering.py
@@ -1,22 +1,22 @@
 import pandas as pd
 import haversine as ha
 import numpy as np
-import time
 
-start = time.time()
+
 #on import le CSV et on le met sous forme de DB pour python
 donnees = pd.read_csv("generated.csv", delimiter=",")
 
-#on calcule les distances dans un tableau triangulaire supérieur
+
 donnees['nb_voisins'] = 0
 donnees['cluster'] = 0
+#donnees['VID'] = []
 
 a = ha.haversine(
     (donnees.loc[0, 'LAT'], donnees.loc[0, 'LON']),  # LAT d'abord !
     (donnees.loc[1, 'LAT'], donnees.loc[1, 'LON']),
 )
 
-traitement = 1000
+traitement = 100
 for i in range (1,(traitement)):
     for j in range (i,traitement):
 
@@ -28,6 +28,17 @@ for i in range (1,(traitement)):
         
         if dist < 45:
             donnees.loc[i,'nb_voisins'] += 1
+            #donnees.loc[i,'VID'].add(j)
+            donnees.loc[j,'nb_voisins'] +=1
+            #donnees.loc[j,'VID'].add(i)
 
-print(donnees)
-print(time.time() - start)
+
+donnees = donnees.sort_values(by=['nb_voisins'], ascending=False)
+#for loop in range(len(donnees )-1):
+#    donnees=pd.DataFrame
+#    if 0 in donnees['cluster'].values:
+
+    
+
+                
+print(donnees) 

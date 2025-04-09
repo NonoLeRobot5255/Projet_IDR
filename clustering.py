19291d1715ea14e3960778d359af46f64d016796
MARTIN Nolann <nolann.martin.2003@gmail.com>
2025-04-09 14:14:11 +0200

    Code wworking for not to much datas

--- clustering.py
+++ clustering.py
@@ -2,18 +2,18 @@ import pandas as pd
 import haversine as ha
 import numpy as np
 
-#on import le CSV et on le met sous forme de DB pour python
+#on import le CSV et on le met sous forme de DB pandas
 donnees = pd.read_csv("generated.csv", delimiter=",")
 
 #on rajoute des colonnes pour nos 3 données suplémentaires
 donnees['nb_voisins'] = 0
 donnees['cluster'] = 0
-donnees['VID'] = [[] for _ in range(len(donnees))]
+donnees['VID'] = [[] for d in range(len(donnees))]
 #le nombre de données qu'on traite
 traitement = 1000
 
 #on calcul ici toutes les distances et les potentiels voisins
-for i in range (1,(traitement)):
+for i in range (traitement):
     for j in range (i+1,traitement):
 
         #on calcule la distance entre les deux points
@@ -33,19 +33,22 @@ for i in range (1,(traitement)):
 
 
 #numéro du premier cluster
-n= 1
+n = 1
+
 #on trie par nombres de voisins potentiels 
-donnees = donnees.sort_values(by=['nb_voisins'], ascending=False)  
+sorted_indices = donnees['nb_voisins'].sort_values(ascending=False).index
+
 
 #on assigne ici les clusters
-for loop in range(traitement):
-    if donnees.loc[loop, 'cluster'] == 0 :
-        donnees.loc[loop, 'cluster'] = n
-        voisins = donnees.loc[loop, 'VID']
+for index in sorted_indices:
+    if donnees.at[index, 'cluster'] == 0:
+        donnees.at[index, 'cluster'] = n
+        voisins = donnees.at[index, 'VID']
         for i in voisins:
-            donnees.loc[i, 'cluster'] = n
+            donnees.at[i, 'cluster'] = n
         n += 1
-
+        
+donnees = donnees.sort_values(by=['nb_voisins'], ascending=False)
 
 #on affiche les données
 print(donnees) 
